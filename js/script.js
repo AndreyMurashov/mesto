379565140e16080c05e71e22f0cc1eec6240224a
Andrey Murashov <amourashov@yandex.ru>
2022-04-07 14:04:15 +0300

    feat: адаптивная верстка

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let popup = document.querySelector('.popup');
 let btnProfileEdit = document.querySelector('.profile__edit');
 let btnPopupClose = document.querySelector('.popup__close');
-let btnPopupSubmit = document.querySelector('.popup__submit');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 let popupData = document.querySelectorAll('.popup__input');
@@ -28,33 +27,4 @@ function formSubmitHandler(evt) {
 btnProfileEdit.addEventListener("click", popupOpen);
 btnPopupClose.addEventListener("click", popupClose)
 formData.addEventListener("submit", formSubmitHandler);
-
-
-
-
-
-// // Находим форму в DOM
-// let formElement = // Воспользуйтесь методом querySelector()
-// // Находим поля формы в DOM
-// let nameInput = // Воспользуйтесь инструментом .querySelector()
-// let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-// // Обработчик «отправки» формы, хотя пока
-// // она никуда отправляться не будет
-// function formSubmitHandler (evt) {
-//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//                                                 // Так мы можем определить свою логику отправки.
-//                                                 // О том, как это делать, расскажем позже.
-
-//     // Получите значение полей jobInput и nameInput из свойства value
-
-//     // Выберите элементы, куда должны быть вставлены значения полей
-
-//     // Вставьте новые значения с помощью textContent
-// }
-
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler); 
-
 
