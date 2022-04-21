b99dcec125031c084261c76da3ef9f79cb9e1adb
Andrey <amourashov@yandex.ru>
2022-04-21 12:43:10 +0300

    промежуточное сохранение

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 let popupDataName = document.querySelector('#name');
 let popupDataStatus = document.querySelector('#status');
-let formData = document.querySelector('.form');
+let formData = document.querySelector('.popup__form');
 
 function popupOpen() {
     popup.classList.add('popup_opened');
@@ -63,50 +63,53 @@ const initialCards = [
 
 
 const elementTemplate = document.querySelector('#element').content;
-const elementsOnline = document.querySelector('.elements');
+let elementsOnline = document.querySelector('.elements');
 
-
+function galeryCreate() {
 for(let i = 0; i < initialCards.length; i++){
 const userElement = elementTemplate.querySelector('.element').cloneNode(true);
 
 userElement.querySelector('.element__image').src = initialCards[i].link;
 userElement.querySelector('.element__name').textContent = initialCards[i].name;
-console.log(userElement);
+
 
 elementsOnline.append(userElement);
 };
+}
+galeryCreate();
 
 ///////////////////////////////////////////////////////////////////////
 
 
+let btnAddPhoto = document.querySelector('.profile__add');
+let popupAddPhoto = document.querySelector('.popup-add-photo');
+let btnPopupAddPhotoClose = document.querySelector('.popup-add-photo__close');
+let addPhotoName = document.querySelector('.profile-add-photo__name');
+let addPhotoLink = document.querySelector('.profile-add-photo__status');
+let popupAddPhotoName = document.querySelector('#photo-name');
+let popupAddPhotoLink = document.querySelector('#photo-link');
+let formDataAddPhoto = document.querySelector('.popup-add-photo__form');
 
-// let popup = document.querySelector('.popup');
-// let btnProfileEdit = document.querySelector('.profile__edit');
-// let btnPopupClose = document.querySelector('.popup__close');
-// let profileName = document.querySelector('.profile__name');
-// let profileStatus = document.querySelector('.profile__status');
-// let popupDataName = document.querySelector('#name');
-// let popupDataStatus = document.querySelector('#status');
-// let formData = document.querySelector('.form');
+function popupAddPhotoOpen() {
+    popupAddPhoto.classList.add('popup-add-photo_opened');
+}
 
-// function popupOpen() {
-//     popup.classList.add('popup_opened');
-//     popupDataName.value = profileName.textContent;
-//     popupDataStatus.value = profileStatus.textContent;
-// }
+function popupAddPhotoClose() {
+    popupAddPhoto.classList.remove('popup-add-photo_opened');
+}
 
-// function popupClose() {
-//     popup.classList.remove('popup_opened');
-// }
-
-// function formSubmitHandler(evt) {
-//     evt.preventDefault();
-//     profileName.textContent = popupDataName.value;
-//     profileStatus.textContent = popupDataStatus.value;
-//     popupClose();
-// }
+function formAddPhotoSubmitHandler(evt) {
+    evt.preventDefault();
+    initialCards.unshift({name: popupAddPhotoName.value, link: popupAddPhotoLink.value});
+    popupAddPhotoClose();
+    popupAddPhotoName.value = '';
+    popupAddPhotoLink.value = '';
+}
 
 
-// btnProfileEdit.addEventListener('click', popupOpen);
-// btnPopupClose.addEventListener('click', popupClose)
-// formData.addEventListener('submit', formSubmitHandler);
+btnAddPhoto.addEventListener('click', popupAddPhotoOpen);
+btnPopupAddPhotoClose.addEventListener('click', popupAddPhotoClose)
+formDataAddPhoto.addEventListener('submit', formAddPhotoSubmitHandler);
+
+
+console.log(initialCards);
